7700124d8d648ea4d3934d77044cc38fce004b43
edxi <edxi@msn.com>
2018-04-14 10:08:43 +0800

    initial

---
+++ src/main/java/com/mylab/helloworld/Helloworld/App.java
@@ -0,0 +1,13 @@
+package com.mylab.helloworld.Helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
